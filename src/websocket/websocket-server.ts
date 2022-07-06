b36fc4ae9c0d05966aa63fd58151dd0636112194
Alex Stelea <alex.s@rdx.works>
2022-07-06 11:33:14 +0200

    remove unused import

--- src/websocket/websocket-server.ts
+++ src/websocket/websocket-server.ts
@@ -4,7 +4,6 @@ import { setToArray } from '../utils/utils'
 import http from 'http'
 
 import client from 'prom-client'
-import { log } from '../utils/log'
 
 const collectDefaultMetrics = client.collectDefaultMetrics
 collectDefaultMetrics()
