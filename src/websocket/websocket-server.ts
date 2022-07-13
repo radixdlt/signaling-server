7ee5f6358d4371d88fa4114f287b59225d3e24c4
Alex Stelea <alex.s@rdx.works>
2022-07-13 09:44:19 +0200

    set number of connected clients

--- src/websocket/websocket-server.ts
+++ src/websocket/websocket-server.ts
@@ -2,6 +2,7 @@ import { connectedClientsGauge } from '../metrics/metrics'
 import { WebSocketServer, WebSocket } from 'ws'
 import { config } from '../config'
 import { setToArray } from '../utils/utils'
+import { log } from '../utils/log'
 
 declare module 'ws' {
   interface WebSocket {
@@ -14,7 +15,11 @@ declare module 'ws' {
 const handleClientHeartbeat = (wss: WebSocketServer) => () => {
   wss.clients.forEach((ws) => {
     if (ws.isAlive === false) {
-      connectedClientsGauge.dec()
+      log.info({
+        event: 'ClientDisconnected',
+        clients: wss.clients.size,
+      })
+      connectedClientsGauge.set(wss.clients.size)
       return ws.terminate()
     }
 
