35f7f0d2333997a316ba4873a57dbd38b239a2e0
Alex Stelea <alex.s@rdx.works>
2022-07-22 15:31:54 +0200

    send client message if websocket is open

--- src/websocket/send-async.ts
+++ src/websocket/send-async.ts
@@ -1,16 +1,23 @@
-import { ResultAsync } from 'neverthrow'
+import { okAsync, ResultAsync } from 'neverthrow'
+import { log } from '../utils/log'
 import { WebSocket } from 'ws'
 
 export const sendAsync = (
   ws: WebSocket,
   message: string
-): ResultAsync<void, Error> =>
-  ResultAsync.fromPromise(
-    new Promise((resolve, reject) => {
-      ws.send(message, (err) => {
-        if (err) return reject(err)
-        resolve(undefined)
-      })
-    }),
-    (error) => error as Error
-  )
+): ResultAsync<void, Error> => {
+  if (ws.readyState === ws.OPEN) {
+    return ResultAsync.fromPromise(
+      new Promise((resolve, reject) => {
+        ws.send(message, (err) => {
+          if (err) return reject(err)
+          resolve(undefined)
+        })
+      }),
+      (error) => error as Error
+    )
+  } else {
+    log.trace({ event: 'WebSocketClosed', state: ws.readyState, message })
+    return okAsync(undefined)
+  }
+}
