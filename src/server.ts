9bdc0c6f99c0255f0551c4ae0b82abf815ab6997
Alex Stelea <alex.s@rdx.works>
2022-07-27 16:30:17 +0200

    revert workers

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,6 @@ import {
 } from './metrics/metrics'
 import './http/http-server'
 import { DataChannelRepo } from './data/data-channel-repo'
-import cluster from 'cluster'
-import { spawn } from './spawn'
 
 const collectDefaultMetrics = prometheusClient.collectDefaultMetrics
 collectDefaultMetrics()
@@ -29,51 +27,44 @@ const server = async () => {
     redis.publish
   )
 
-  if (cluster.isPrimary) {
-    spawn()
-  } else {
-    wss.on('connection', (ws) => {
-      log.debug({ event: `ClientConnected`, clients: wss.clients.size })
-      connectedClientsGauge.set(wss.clients.size)
+  wss.on('connection', (ws) => {
+    log.debug({ event: `ClientConnected`, clients: wss.clients.size })
+    connectedClientsGauge.set(wss.clients.size)
 
+    ws.isAlive = true
+
+    ws.on('pong', () => {
       ws.isAlive = true
+    })
 
-      ws.on('pong', () => {
-        ws.isAlive = true
-      })
+    ws.onmessage = async (event) => {
+      try {
+        incomingMessageCounter.inc()
+        const result = await handleIncomingMessage(ws, event.data.toString())
 
-      ws.onmessage = (event) => {
-        try {
-          incomingMessageCounter.inc()
-          handleIncomingMessage(ws, event.data.toString()).mapErr((error) => {
-            if (
-              [
-                'write EPIPE',
-                'Cannot call write after a stream was destroyed',
-              ].includes(error.message)
-            )
-              return
-            log.error(error)
-          })
-        } catch (error) {
+        if (result.isErr()) {
+          const error = result.error
+          if (error.message === 'write EPIPE') return
           log.error(error)
         }
+      } catch (error) {
+        console.error(error)
       }
+    }
 
-      ws.onerror = (event) => {
-        log.error(event.error)
-      }
+    ws.onerror = (event) => {
+      log.error(event.error)
+    }
 
-      ws.onclose = () => {
-        connectedClientsGauge.set(wss.clients.size)
-        log.trace({
-          event: 'ClientDisconnected',
-          clients: wss.clients.size,
-        })
-        dataChannelRepo.remove(ws)
-      }
-    })
-  }
+    ws.onclose = () => {
+      connectedClientsGauge.set(wss.clients.size)
+      log.trace({
+        event: 'ClientDisconnected',
+        clients: wss.clients.size,
+      })
+      dataChannelRepo.remove(ws)
+    }
+  })
 }
 
 const runServer = async () => {
